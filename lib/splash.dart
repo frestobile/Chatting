14c9e34b583e157c15003d7dff64821c05fe2495
frstobile <forever_ourvictory@hotmail.com>
2024-08-30 19:44:03 -0700

    Initial commit

---
+++ lib/splash.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'auth.dart'; 
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) => AuthScreen()),  // Navigate to AuthScreen after 3 seconds
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset('assets/splash.png', width: 150, height: 150),
+      ),
+    );
+  }
+}
